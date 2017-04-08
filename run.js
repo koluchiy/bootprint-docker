c98af46ddb4bf376a68d46ee71792f9add809e96
nrybin <n.rybin@carprice.ru>
2017-04-08 13:59:46 +0300

    minify

--- run.js
+++ run.js
@@ -1,11 +1,18 @@
 var boot = require('bootprint');
 var fs = require('fs');
 
-var target = '/bootprint/target';
-
 if (process.argv.length < 3) {
   console.log('Usage: node.js run.js [swagger-file]');
   process.exit(1);
+}
+
+var target = '/bootprint/target';
+var swagger = process.argv[2];
+
+var outfile = 'out.html';
+
+if (process.argv.length > 3) {
+  outfile = process.argv[3];
 }
 
 // Load bootprint-swagger 
@@ -13,7 +20,7 @@ boot.load(require('bootprint-openapi'))
 // Customize configuration, override any options 
 .merge({ /* Any other configuration */})
 // Specify build source and target 
-.build(process.argv[2], target)
+.build(swagger, target)
 // Generate swagger-documentation into "target" directory 
 .generate()
 .done(inlineCallback);
@@ -27,8 +34,30 @@ function inline(dir, target) {
 
 
   input.pipe(inline).pipe(output);
+
+  fs.readFile(target, 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+
+    var minify = require('html-minifier').minify;
+    var result = minify(data, {
+      minifyCSS: true,
+      minifyJS: true,
+      preserveLineBreaks: true,
+      removeComments: true,
+      collapseWhitespace: true,
+      removeAttributeQuotes: true,
+      collapseBooleanAttributes: true,
+      removeEmptyAttributes: true
+    });
+
+    fs.writeFile(target, result, function(err) {
+      console.log(err);
+    });
+  });
 }
 
 function inlineCallback() {
-  inline(target, '/tmp/bootprint/out.html');
+  inline(target, '/tmp/bootprint/' + outfile);
 }
